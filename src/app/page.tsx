d509c16ebc77214e8ee43abf0cf04ca6c2d830f0
kopsus <emonsetio01@gmail.com>
2024-09-17 13:37:38 +0700

    add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { Suspense } from "react";
 import Cover from "@/components/cover/Cover";
 export default function Page() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Cover />
-    </>
+    </Suspense>
   );
 }
