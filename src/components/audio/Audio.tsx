3e60e47fce843ccce395bd071c115a5b7efacefa
kopsus <emonsetio01@gmail.com>
2024-09-17 22:20:19 +0700

    change icon play and pause

--- src/components/audio/Audio.tsx
+++ src/components/audio/Audio.tsx
@@ -20,7 +20,7 @@ const Audio = ({ toggleAudio, isPlaying, audioRef, setIsPlaying }: IAudio) => {
         className="z-50 fixed bottom-10 right-[50px] md:right-1/3 w-8 h-8 p-1 rounded-full bg-white flex justify-center items-center text-white cursor-pointer"
         onClick={toggleAudio}
       >
-        {!isPlaying ? <FaPause color="#000" /> : <FaMusic color="#000" />}
+        {!isPlaying ? <FaMusic color="#000" /> : <FaPause color="#000" />}
       </div>
       <audio
         ref={audioRef}
